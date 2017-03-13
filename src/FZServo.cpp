fd6db70348819abc2c6e6a6bc387cc0c2ac700ad
zwanh <xilinxmcu@gmail.com>
2017-03-13 09:53:32 +0800

    modified for NPN transistor

--- src/FZServo.cpp
+++ src/FZServo.cpp
@@ -2,61 +2,17 @@
 
 //for the situation of only one enable pin.when power on,the host should be in recieve mode when tx enabled
 //by low voltage level while rx enabled by high;otherwise you should set reverse true.
-FZServo::FZServo(HardwareSerial *serial, word baudrate, byte enable, bool reverse=false)
+FZServo::FZServo(HardwareSerial *serial, long timeout)
 {
   svSer = serial;
-  svSer->begin(baudrate);
-  svSer->setTimeout(25);
-
-  twoPinsCtrl = false;
-
-  _rxEn = enable;
-  _rxRvs = reverse;
-
-  pinMode(_rxEn, OUTPUT);
-  digitalWrite(_rxEn, !_rxRvs);
-}
-
-
-FZServo::FZServo(HardwareSerial *serial, word baudrate, byte txEn, byte rxEn, bool txRvs=false, bool rxRvs=false)
-{
-  svSer = serial;
-  svSer->begin(baudrate);
-  svSer->setTimeout(25);
-
-  twoPinsCtrl = true;
-
-  _txEn = txEn;
-  _txRvs = txRvs;
-  _rxEn = rxEn;
-  _rxRvs = rxRvs;
-
-  pinMode(_txEn, OUTPUT);
-  digitalWrite(_txEn, _txRvs);
-  pinMode(_rxEn, OUTPUT);
-  digitalWrite(_rxEn, !_rxRvs);
-}
-
-
-void FZServo::changeToWrite()
-{
-  digitalWrite(_rxEn, _rxRvs);
-  if (twoPinsCtrl)
-    digitalWrite(_txEn, !_txRvs);
-}
-
-
-void FZServo::changeToRead()
-{
-  if (twoPinsCtrl)
-    digitalWrite(_txEn, _txRvs);
-  digitalWrite(_rxEn, !_rxRvs);
+  svSer -> begin(115200);
+  svSer -> setTimeout(timeout);
 }
 
 
 byte FZServo::checkSum(byte *buf, byte len)
 {
-  byte sum;
+  byte sum = 0;
   for(byte cnt=2; cnt<len; cnt++){
     sum += buf[cnt];
   }
@@ -64,39 +20,93 @@ byte FZServo::checkSum(byte *buf, byte len)
 }
 
 
-byte FZServo::ping(byte id)
+boolean FZServo::ping(byte id, byte *data)
 {
-  byte buf[6] = {0xff, 0xff};
-  buf[2] = id;
-  buf[3] = 0x02;
-  buf[4] = 0x01;
-  buf[5] = checkSum(buf, 5);
+  byte buf[6] = {0xff, 0xff, id, 0x2, 0x1, ~(id+3)};
 
-  changeToWrite();
+  while(svSer -> availableForWrite() < 6);
   svSer -> write(buf, 6);
   svSer -> flush();
 
-  changeToRead();
   byte cnt = svSer -> readBytes(buf, 6);
-  if (cnt == 6 && buf[1] == 0xf5) {
-    if (checkSum(buf, 5) == buf[5])
-      return buf[4];
+  if (cnt == 6 && buf[0] == 0xff && buf[1] == 0xf5
+      && buf[2] == id && buf[3] == 0x2 &&
+      checkSum(buf, 5) == buf[5]){
+    *data = buf[4];
+    return true;
   }
-  return 0xff;
+  return false;
+}
+
+
+boolean FZServo::read(byte id, byte regStartAddr, byte *data, byte len)
+{
+  byte writeBuf[8] = {0xff, 0xff, id, 0x4, 0x2, regStartAddr,
+                  len, ~(id+regStartAddr+len+6)};
+
+  while(svSer -> availableForWrite() < 8);
+  svSer -> write(writeBuf, 8);
+  svSer -> flush();
+
+  byte readBuf[len + 6];
+  byte cnt = svSer -> readBytes(readBuf, len + 6);
+
+  if (cnt == len + 6 && readBuf[0] == 0xff && readBuf[1] == 0xf5
+      && readBuf[2] == id && readBuf[3] == len + 2 &&
+      checkSum(readBuf, len + 5) == readBuf[len + 5]){
+
+    memcpy(data, &readBuf[5], len);
+    return true;
+  }
+  return false;
 }
 
 
 void FZServo::reset(byte id)
 {
-  byte buf[6] = {0xff, 0xff};
-  buf[2] = id;
-  buf[3] = 0x02;
-  buf[4] = 0x06;
-  buf[5] = checkSum(buf, 5);
+  byte buf[6] = {0xff, 0xff, id, 0x2, 0x6, ~(id+8)};
 
-  changeToWrite();
+  while(svSer -> availableForWrite() < 6);
   svSer -> write(buf, 6);
   svSer -> flush();
+}
 
-  changeToRead();
+
+
+void FZServo::write(byte id, byte regStartAddr, byte *buf, byte bufLen)
+{
+  byte writeBufLen = bufLen + 7;
+  byte writeBuf[writeBufLen] = {0xff, 0xff, id, bufLen+3, INSTRUCTION_WRITE_DATA, regStartAddr};
+  memcpy(&writeBuf[6], buf, bufLen);
+  writeBuf[writeBufLen - 1] = checkSum(writeBuf, writeBufLen - 1);
+
+  while(svSer -> availableForWrite() < writeBufLen);
+  svSer -> write(writeBuf, writeBufLen);
+  svSer -> flush();
+}
+
+
+//mode: true sync;  false async
+void FZServo::write(byte regStartAddr, byte *buf, byte svNum, byte perDataLen, boolean mode)
+{
+  byte writeBufLen = perDataLen * svNum + 8;
+  byte writeBuf[writeBufLen] = {0xff, 0xff, BROADCAST_ADDR, writeBufLen - 4,
+                                mode ? INSTRUCTION_SYNC_WRITE : INSTRUCTION_REG_WRITE,
+                                regStartAddr, perDataLen - 1};
+  memcpy(&writeBuf[7], buf, writeBufLen - 8);
+  writeBuf[writeBufLen - 1] = checkSum(writeBuf, writeBufLen - 1);
+
+  while(svSer -> availableForWrite() < writeBufLen);
+  svSer -> write(writeBuf, writeBufLen);
+  svSer -> flush();
+}
+
+
+void FZServo::act()
+{
+  byte buf[6] = {0xff, 0xff, BROADCAST_ADDR, 0x2, 0x5, 0xfa};
+
+  while(svSer -> availableForWrite() < 6);
+  svSer -> write(buf, 6);
+  svSer -> flush();
 }
--- src/FZServo.h
+++ src/FZServo.h
@@ -3,28 +3,52 @@
 
 #include <Arduino.h>
 
+#define BROADCAST_ADDR  0xFE
+
+//the macros below is instructions
+#define INSTRUCTION_PING        0x1
+#define INSTRUCTION_READ_DATA   0x2
+#define INSTRUCTION_WRITE_DATA  0x3
+#define INSTRUCTION_REG_WRITE   0x4
+#define INSTRUCTION_ACTION      0x5
+#define INSTRUCTION_RESET       0x6
+#define INSTRUCTION_SYNC_WRITE  0x83
+
+
+//the macros below is storaged in EEPROM
+#define ID_REG			0x5
+#define MIN_ANGLE_LIMIT_H	0x9
+#define MIN_ANGLE_LIMIT_L	0xA
+#define MAX_ANGLE_LIMIT_H	0xB
+#define MAX_ANGLE_LIMIT_L	0xC
+#define MAX_TORQUE_H		0x10
+#define MAX_TORQUE_L		0x11
+#define DEFAULT_SPEED		0x12
+#define MIDDLE_POSITION_H	0x14
+#define MIDDLE_POSITION_L	0x15
+
+//the macros below is storaged in RAM
+#define TORQUE_SWITCH		0x28
+#define TARGET_POSITION_H	0x2A
+#define TARGET_POSITION_L	0x2B
+#define CURRENT_POSITION_H  0x38
+#define CURRENT_POSITION_L  0x39
+#define TARGET_SPEED		0x41
+
+
 class FZServo
 {
   private:
-
-    //for uart control
-    bool twoPinsCtrl;
-    bool _rxRvs;
-    bool _txRvs;
-    byte _rxEn;
-    byte _txEn;
-
     HardwareSerial *svSer;
-
-    void changeToWrite();
-    void changeToRead();
-
     byte checkSum(byte *buf, byte len);
 
   public:
-    FZServo(HardwareSerial *serial, word baudrate, byte enable, bool reverse=false);
-    FZServo(HardwareSerial *serial, word baudrate, byte txEn, byte rxEn, bool txRvs=false, bool rxRvs=false);
-    byte ping(byte id);
+    FZServo(HardwareSerial *serial, long timeout);
+    boolean ping(byte id, byte *data);
+    boolean read(byte id, byte regStartAddr, byte *data, byte len);
+    void write(byte id, byte regStartAddr, byte *buf, byte bufLen);
+    void write(byte regStartAddr, byte *buf, byte svNum, byte perDataLen, boolean mode);
+    void act();
     void reset(byte id);
 };
 
